b7d299350b0c2692b5c895ae59b3d3c4457df085
Zuskina <iliwhaleli@mail.ru>
2018-05-22 12:08:26 +0600

    lab-6 v1.0

---
+++ 6/RemoveFromArray.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab3
+{
+    public class RemoveFromArray
+    {
+        public List<double> DeleteElement(List<double> array)
+        {
+            double average = ArithmeticMean(array);
+
+            for (int i = array.Count - 1; i >= 0; i--)
+            {
+                if (array[i] > average)
+                {
+                    array.Remove(array[i]);
+                }
+            }
+
+            return array;
+        }
+    }
+}
