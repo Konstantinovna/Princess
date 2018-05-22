9d669bcb0a4d0582fac6a1220494cba050bb0cb9
Zuskina <iliwhaleli@mail.ru>
2018-05-22 12:09:42 +0600

    lab-9 v1.0

---
+++ 9/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace laba4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            static void Main(string[] args)
+            {
+                int[,] array = new int[5, 5];
+                Random random = new Random();
+
+                Sort sortingArray = new Sort();
+
+                Console.Write("Исходный массив: \n");
+                for (int i = 0; i < array.GetLength(0); i++)
+                {
+                    for (int j = 0; j < array.GetLength(1); j++)
+                    {
+                        array[i, j] = random.Next(0, 9);
+                        Console.Write(array[i, j] + " ");
+                    }
+                    Console.WriteLine();
+                }
+
+                array = sortingArray.SortingArray(array);
+
+                Console.Write("\nОтсортированный массив: \n");
+                for (int i = 0; i < array.GetLength(0); i++)
+                {
+                    for (int j = 0; j < array.GetLength(1); j++)
+                    {
+                        Console.Write(array[i, j] + " ");
+                    }
+                    Console.WriteLine();
+                }
+
+                Console.ReadLine();
+            }
+    }
+}
---
+++ 9/index.jsp
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab3
+{
+    class Sort
+    {
+
+        private List<int> oneDimensionalArray = new List<int>();
+
+        public int[,] SortingArray(int[,] array)
+        {
+            Regularize(array);
+            int m = 0;
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    array[i, j] = oneDimensionalArray[m];
+                    m++;
+                }
+            }
+            return array;
+        }
+
+        private List<int> Regularize(int[,] array)
+        {
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    oneDimensionalArray.Add(array[i, j]);
+                }
+            }
+            oneDimensionalArray.Sort();
+            return oneDimensionalArray;
+        }
+    }
+}
