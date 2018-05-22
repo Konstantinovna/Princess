68bc8ade5fab193f6cb79c63fa4a1753422866c9
Zuskina <iliwhaleli@mail.ru>
2018-05-22 12:05:54 +0600

    lab-3 v1.0

---
+++ 3/OddEvenSplitter.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab3
+{
+    public class OddEvenSplitter
+    {
+        public List<int> ExtractOdd(int[] array)
+        {
+            List<int> result = new List<int>();
+            for (int n = 0; n < array.Length; n++)
+            {
+                if (array[n] % 2 != 0)
+                {
+                    result.Add(array[n]);
+                }
+            }
+
+            return result;
+        }
+
+        public List<int> ExtractEven(int[] array)
+        {
+            List<int> result = new List<int>();
+            for (int n = 0; n < array.Length; n++)
+            {
+                if (array[n] % 2 == 0 && array[n] != 0)
+                {
+                    result.Add(array[n]);
+                }
+            }
+
+            return result;
+        }
+
+    }
+}
