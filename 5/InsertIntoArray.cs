ffd84dc1fbb3ff89e1daf7ca9136319c26cf62b8
Zuskina <iliwhaleli@mail.ru>
2018-05-22 12:07:36 +0600

    lab-5 v1.0

---
+++ 5/InsertIntoArray.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab3
+{
+    public class InsertIntoArray
+    {
+        public int[] Divider(int[] array)
+        {
+            int sum = Math.Abs(array[0]) + Math.Abs(array[1]) + Math.Abs(array[array.Length - 1]);
+            Console.WriteLine($"Сумма первого, второго и последнего элементов: {sum}");
+
+            string[] inString = Console.ReadLine().Split();
+            string outString = " ";
+
+
+            //проверка типа данных
+            for (int i = 0; i <= array.Length - 1; i++)
+            {
+                if (!int.TryParse(inString[i], out array[i]))
+                {
+                    Console.ReadKey();
+                }
+            }
+
+            for (int i = 0; i <= array.Length - 1; i++)
+            {
+                outString = outString + inString[i] + " ";
+                if (sum % array[i] == 0)
+                {
+                    outString = outString + "0 ";
+                }
+            }
+
+            return array;
+        }
+    }
+}
+
---
+++ 5/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace laba4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите массив: ");
+            string[] inString = Console.ReadLine().Split();
+            string outString = " ";
+            int[] array = new int[inString.Length];
+
+
+            InsertIntoArray spliter = new InsertIntoArray();
+            var result = spliter.Divider(array);
+
+
+            //проверка типа данных
+            for (int i = 0; i <= result.Length - 1; i++)
+            {
+                if (!int.TryParse(inString[i], out array[i]))
+                {
+                    Console.ReadKey();
+                }
+            }
+
+
+            for (int i = 0; i <= result.Length - 1; i++)
+            {
+                Console.Write(result[i]);
+            }
+
+            Console.WriteLine($"Измененный массив: {outString.Trim()}");
+            Console.ReadLine();
+        }
+    }
+}
