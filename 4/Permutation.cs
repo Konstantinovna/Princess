5494bd4b4cb9211f66570cfe00736c214e0ff940
Zuskina <iliwhaleli@mail.ru>
2018-05-22 12:07:03 +0600

    lab-4 v1.0

---
+++ 4/Permutation.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab3
+{
+    public class Permutation
+    {
+        public int[] Change(int[] array)
+        {
+            int temp = array[2];
+            array[2] = array[4];
+            array[4] = temp;
+
+            return array;
+        }
+    }
+}
---
+++ 4/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace laba4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите количество элементов");
+            int size = Convert.ToInt32(Console.ReadLine());
+            int[] array = new int[size];
+
+            Console.WriteLine("Исходный массив");
+            Random rand = new Random();
+            for (int i = 0; i < size; i++)
+            {
+                array[i] = rand.Next(1, 6);
+            }
+
+            foreach (int k in array)
+                Console.Write(k);
+
+            Permutation spliter = new Permutation();
+            var result = spliter.Change(array);
+
+            Console.WriteLine("\nИзмененный массив");
+
+            for (int m = 0; m < result.Length; m++)
+            {
+                Console.Write(result[m]);
+            }
+
+
+            Console.ReadLine();
+        }
+    }
+}
