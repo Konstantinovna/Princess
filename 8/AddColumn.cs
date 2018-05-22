a35043afbaec333c903310e028f6a98afb9edc25
Zuskina <iliwhaleli@mail.ru>
2018-05-22 12:09:26 +0600

    lab-8 v1.0

---
+++ 8/AddColumn.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab3
+{
+    class AddColumn
+    {
+        public int[,] Array(int[,] array, int min, int column, int m, int n)
+        {
+
+            for (int i = 0; i < m; i++)
+                for (int j = 0; j < n; j++)
+                {
+                    if (array[i, j] < min)
+                    {
+                        min = array[i, j];
+                        column = j;
+                    }
+                    return array;
+                }
+        }
+
+        public int[,] print(int[,] array, int column, int m, int n)
+        {
+
+            for (int i = 0; i < m; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    if (j == column) continue;
+                    Console.Write(array[i, j]);
+                    Console.Write('\t');
+                }
+                Console.WriteLine();
+                return array;
+            }
+        }
+    }
+}
---
+++ 8/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace laba4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+                    private static Random rnd = new Random();
+
+        static void Main(string[] args)
+        {
+
+
+            Console.WriteLine("\nИсходный массив: ");
+            AddColumn spliter = new AddColumn();
+
+
+            int[,] array = new int[5, 5];
+            Random random = new Random();
+
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    array[i, j] = random.Next(-9, 9);
+                }
+                Console.WriteLine();
+            }
+            spliter.print(array);
+
+            var result = spliter.Array(array);
+
+
+            int min = array[0, 0];
+            int column = 0;
+
+            for (int i = 0; i < m; i++)
+                for (int j = 0; j < n; j++)
+                {
+                    if (array[i, j] < min)
+                    {
+                        min = array[i, j];
+                        column = j;
+                    }
+                }
+
+            Console.WriteLine($"\nМинимальное отрицательное: {min} в столбце {column + 1}");
+            Console.WriteLine("\nИзмененный массив: ");
+
+            for (int i = 0; i < m; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    if (j == column) continue;
+                    Console.Write(array[i, j]);
+                    Console.Write('\t');
+                }
+                Console.WriteLine();
+            }
+
+            Console.ForegroundColor = ConsoleColor.White;
+
+            Console.ReadKey();
+        }
+    }
+}
