f03d5e84098491148dcc45597ac68aa5801fe095
Zuskina <iliwhaleli@mail.ru>
2018-05-22 12:14:04 +0600

    lab-1 v1.0

---
+++ 1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            SeparatingDigits stringNumbers = new SeparatingDigits();
+            Console.WriteLine("Введите шестизначное число: ");
+            int number = int.Parse(Console.ReadLine());
+            string result = stringNumbers.separation(number);
+            Console.WriteLine($"Сумма первых 3 десятичных цифр {result} сумме 3 последних десятичных цифр");
+            Console.Read();
+        }
+    }
+}
---
+++ 1/SeparatingDigits.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp8
+{
+    class SeparatingDigits
+    {
+        public string separation(int number)
+        {
+            string result = "не равна";
+            int firstDigit = number / 100000;
+            int secondDigit = (number / 10000) % 10;
+            int thirdDigit = (number / 1000) % 10;
+            int forthDigit = (number / 100) % 10;
+            int fifthDigit = (number / 10) % 10;
+            int sixthDigit = number % 10;
+            if ((firstDigit + secondDigit + thirdDigit) == (forthDigit + fifthDigit + sixthDigit))
+            {
+                result = "равна";
+            }
+            return result;
+        }
+        }
+}
