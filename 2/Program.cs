a249fb043cdadc39c78e2f234a973c0a7bda1c0a
Zuskina <iliwhaleli@mail.ru>
2018-05-22 12:11:05 +0600

    lab-2 v1.0

---
+++ 2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace laba4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            string[] splited = input.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+            string tmp;
+            for (int i = 0; i < splited.Length; i++)
+            {
+                tmp = splited[i];
+                if (!char.IsDigit(Convert.ToChar(tmp.Substring(0, 1))))
+                {
+                    splited[i] = tmp.Substring(0, 1).ToUpper() + tmp.Substring(1, tmp.Length - 1);
+                }
+            }
+            string result = "";
+            foreach (string s in splited)
+            {
+                result += s + " ";
+            }
+            Console.Write(result);
+        }
+    }
+}
---
+++ 2/Test.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab3
+{
+    class Test
+    {
+        static void Main(string[] args)
+        {
+            string str = "привет как твои дела";
+            string[] array = str.Split(' ');
+            StringBuilder sb = new StringBuilder();
+            foreach (string item in array)
+            {
+                sb.Append(char.ToUpper(item[0]) + item.Substring(1, item.Length - 1) + " ");
+            }
+            Console.WriteLine(sb.ToString());
+        }
+    }
+}
