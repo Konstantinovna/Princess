795e987d40d4b5c5140e4a2baea28046de3b7788
Zuskina <iliwhaleli@mail.ru>
2018-05-22 12:10:28 +0600

    lab-10 v1.0

---
+++ 10/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace laba4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AddZero addZero = new AddZero();
+            List<double> array = new List<double>();
+            StreamReader read = new StreamReader("F:\\ПТИПО\\10\\input.txt");
+
+            string line;
+
+            while ((line = read.ReadLine()) != null)
+            {
+                array.Add(Convert.ToDouble(line));
+            }
+
+            array = addZero.addZero(array);
+
+            StreamWriter write = new StreamWriter("F:\\ПТИПО\\10\\output.txt");
+
+            for (int i = 0; i < array.Count; i++)
+            {
+                write.Write(array[i] + " ");
+            }
+
+            write.Close();
+
+            Console.Read();
+        }
+    }
+}
